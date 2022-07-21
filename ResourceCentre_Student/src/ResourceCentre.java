33a05d15a8c270a0edae8aff2b7909ba5653a050
21045050 <21045050@10.174.115.62>
2022-07-21 14:39:26 +0800

    update

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -149,11 +149,9 @@ public class ResourceCentre {
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
 		// write your code here
-	
-
 		for (int i = 0; i < chromebookList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", chromebookList.get(i).getAssetTag(),
+			output += String.format("%-10s %-30s %-10s %-10s %-20s\n", chromebookList.get(i).getAssetTag(),
 					chromebookList.get(i).getDescription(), 
 					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
 					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
@@ -161,14 +159,6 @@ public class ResourceCentre {
 		}
 		return output;
 	}
-		
-		
-		
-		
-		
-	
-	
-	
 	
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
 		
